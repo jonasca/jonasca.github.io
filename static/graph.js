97120bdd6ba6ce5dcff7448dbfec2a31cfded035
Jonas Castro <jonasgdecastro@gmail.com>
2016-08-29 16:47:15 -0700

    Add files via upload

---
+++ static/graph.js
@@ -0,0 +1,157 @@
+d3.queue()
+    .defer(d3.json, "/data")
+    .await(makeGraphs); // Queue lets data load completely before creating charts
+    
+function makeGraphs(error, data) {
+    
+    //Clean data
+    var records = data;
+        
+    records.forEach(function(d) {
+        
+        d.timestamp = new Date(d.timestamp / 1000000)    
+        d["timestamp"].setMinutes(0);
+        d["timestamp"].setSeconds(0);
+        d.longitude = +d.longitude;
+        d.latitude = +d.latitude;
+    });
+    
+    // Good place to check if datatime worked, with console.log(records[0].timestamp); console.log("everything ran");
+    
+    
+ 
+    //Create a Crossfilter instance, used for multi-dimensional data manipulation in js
+	var ndx = crossfilter(records);
+    
+	//Define Dimensions
+	var dateDim = ndx.dimension(function(d) { return d["timestamp"]; });
+	var genderDim = ndx.dimension(function(d) { return d["gender"]; });
+	var ageSegmentDim = ndx.dimension(function(d) { return d["age_segment"]; });
+	var phoneBrandDim = ndx.dimension(function(d) { return d["phone_brand_en"]; });
+	var locationdDim = ndx.dimension(function(d) { return d["location"]; });
+	var allDim = ndx.dimension(function(d) {return d;});
+    
+    //Group Data
+	var numRecordsByDate = dateDim.group();
+	var genderGroup = genderDim.group();
+	var ageSegmentGroup = ageSegmentDim.group();
+	var phoneBrandGroup = phoneBrandDim.group();
+	var locationGroup = locationdDim.group();
+	var all = ndx.groupAll();
+    
+    //Define values (to be used in charts)
+	var minDate = dateDim.bottom(1)[0]["timestamp"];
+	var maxDate = dateDim.top(1)[0]["timestamp"];
+
+    //Charts
+    var numberRecordsND = dc.numberDisplay("#number-records-nd");
+	var timeChart = dc.barChart("#time-chart");
+	var genderChart = dc.rowChart("#gender-row-chart");
+	var ageSegmentChart = dc.rowChart("#age-segment-row-chart");
+	var phoneBrandChart = dc.rowChart("#phone-brand-row-chart");
+	var locationChart = dc.rowChart("#location-row-chart");
+    
+    console.log(records[0].timestamp);
+    console.log("everything ran");
+    
+    
+    numberRecordsND
+		.formatNumber(d3.format("d"))
+		.valueAccessor(function(d){return d; })
+		.group(all);
+        
+	timeChart
+		.width(650)
+		.height(150)
+		.margins({top: 10, right: 50, bottom: 20, left: 20})
+		.dimension(dateDim)
+		.group(numRecordsByDate)
+		.transitionDuration(500)
+		.x(d3.time.scale().domain([minDate, maxDate]))
+		.elasticY(true)
+		.yAxis().ticks(4);
+
+	genderChart
+        .width(300)
+        .height(100)
+        .dimension(genderDim)
+        .group(genderGroup)
+        .ordering(function(d) { return -d.value })
+        .ordinalColors(['#6baed6'])
+        .elasticX(true)
+        .xAxis().ticks(4);
+
+	ageSegmentChart
+		.width(300)
+		.height(190)
+        .dimension(ageSegmentDim)
+        .group(ageSegmentGroup)
+        .ordinalColors(['#6baed6'])
+        .elasticX(true)
+        .labelOffsetY(10)
+        .xAxis().ticks(4);
+        
+    phoneBrandChart
+		.width(300)
+		.height(350)
+        .dimension(phoneBrandDim)
+        .group(phoneBrandGroup)
+        .ordering(function(d) { return -d.value })
+        .ordinalColors(['#6baed6'])
+        .elasticX(true)
+        .xAxis().ticks(4);
+
+    locationChart
+    	.width(200)
+		.height(560)
+        .dimension(locationdDim)
+        .group(locationGroup)
+        .ordering(function(d) { return -d.value })
+        .ordinalColors(['#6baed6'])
+        .elasticX(true)
+        .rowsCap(27) // Known dc bug, this is a work-around to ameliorate tick alighnment https://github.com/dc-js/dc.js/issues/669
+        .labelOffsetY(10)
+        .xAxis().ticks(4);
+        
+
+    var map = L.map('map');
+
+	var drawMap = function(){
+
+	    map.setView([31.75, 110], 4);
+		mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+		L.tileLayer(
+			'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+				attribution: '&copy; ' + mapLink + ' Contributors',
+				maxZoom: 15,
+			}).addTo(map);
+
+		//HeatMap
+		var geoData = [];
+		_.each(allDim.top(Infinity), function (d) {
+			geoData.push([d["latitude"], d["longitude"], 1]);
+	      });
+		var heat = L.heatLayer(geoData,{
+			radius: 10,
+			blur: 20, 
+			maxZoom: 1,
+		}).addTo(map);
+    }; 
+    
+    //Draw Map
+	drawMap();
+
+	//Update the heatmap if any dc chart get filtered
+	dcCharts = [timeChart, genderChart, ageSegmentChart, phoneBrandChart, locationChart];
+
+	_.each(dcCharts, function (dcChart) {
+		dcChart.on("filtered", function (chart, filter) {
+			map.eachLayer(function (layer) {
+				map.removeLayer(layer)
+			}); 
+			drawMap();
+		});
+	});
+
+	dc.renderAll();
+};
