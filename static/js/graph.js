bfaf8cdd7510d4b6827947f931346aa5ba9e9d54
Jonas Castro <jonasgdecastro@gmail.com>
2016-08-30 13:12:15 -0700

    Update graph.js

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,5 +1,5 @@
 d3.queue()
-    .defer(d3.json, "https://jonasca.github.io/dashboad_data.json")
+    .defer(d3.json, {{ site.data.dashboard_data | jsonify }})
     .await(makeGraphs); // Queue lets data load completely before creating charts
     
 function makeGraphs(error, data) {
