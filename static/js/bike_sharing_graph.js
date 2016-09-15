989ba2bdc06e4be6e6ea8007f76dc98eb2ad5e0b
Jonas Castro <jonasgdecastro@gmail.com>
2016-09-14 18:29:25 -0700

    Update bike_sharing_graph.js

--- static/js/bike_sharing_graph.js
+++ static/js/bike_sharing_graph.js
@@ -9,11 +9,6 @@ function makeGraphs(error, data) {
     
     records.forEach(function(d) {
         
-        //d.timestamp = new Date(d.timestamp)    // bc d3.json  is returning date in string format
-        //d["timestamp"].setMinutes(0);
-        //d["timestamp"].setSeconds(0);
-        //d.longitude = +d.longitude;
-        //d.latitude = +d.latitude;
     });
     
     console.log(records[0]);
