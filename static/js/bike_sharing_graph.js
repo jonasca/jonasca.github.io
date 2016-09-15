b9760f3ea7bae6cf7c24f4983ed6eab742b7d8c0
Jonas Castro <jonasgdecastro@gmail.com>
2016-09-14 18:40:24 -0700

    Update bike_sharing_graph.js

--- static/js/bike_sharing_graph.js
+++ static/js/bike_sharing_graph.js
@@ -8,10 +8,33 @@ function makeGraphs(error, data) {
     var records = data;
     
     records.forEach(function(d) {
-        
+        // Space for transforming data - nothing as of now
     });
     
     console.log(records[0]);
     // Good place to check if datatime worked, with console.log(records[0].timestamp); console.log("everything ran");
+    
+    //Create a Crossfilter instance, used for multi-dimensional data manipulation in js
+	var ndx = crossfilter(records);
+    
+	//Define Dimensions
+	var dateDim = ndx.dimension(function(d) { return d["datetime"]; });
+	var yearDim = ndx.dimension(function(d) { return d["year"]; });
+	var monthDim = ndx.dimension(function(d) { return d["month"]; });
+	var dayDim = ndx.dimension(function(d) { return d["day"]; });
+	var weekdayDim = ndx.dimension(function(d) { return d["weekday"]; });
+	var hourDim = ndx.dimension(function(d) { return d["hour"]; });
+	var seasonDim = ndx.dimension(function(d) { return d["season"]; });
+	var weatherDim = ndx.dimension(function(d) { return d["weather"]; });
+	var allDim = ndx.dimension(function(d) {return d;});
+	
+	// Gotta change these guys to match talking data and the above.
+	//Group Data
+	//var numRecordsByDate = dateDim.group();
+	//var genderGroup = genderDim.group();
+	//var ageSegmentGroup = ageSegmentDim.group();
+	//var phoneBrandGroup = phoneBrandDim.group();
+	//var locationGroup = locationdDim.group();
+	var all = ndx.groupAll();
     
 };
